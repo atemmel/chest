719760c30755c0223e428e4ccb2910189ec860d8
atemmel <temmeladam@gmail.com>
2023-08-17 18:04:20 +0200

    improved nothing

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"text/template"
+)
+
+var (
+	templates = map[string]string{
+		"/": "template/index.html",
+	}
 )
 
 func main() {
 	http.Handle("/static/", http.FileServer(http.Dir("./")))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		view := template.Must(template.ParseFiles("index.html"))
-		view.Execute(w, nil)
-	})
+	for k, v := range templates {
+		http.HandleFunc(k, func(w http.ResponseWriter, r *http.Request) {
+			view := template.Must(template.ParseFiles(v))
+			view.Execute(w, nil)
+		})
+	}
 
-	http.ListenAndServe(":8080", nil)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
